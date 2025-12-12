5abd132c05a69b297a6b88cbaf376eea49078796
Gabriel <usr4035@salesianos-lacuesta.net>
2025-12-12 17:38:19 +0000

    feat:Se agrego el drawer sin yocken y redirigir a login

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,27 @@
+
+import { useEffect, useState } from "react";
 import { Drawer } from "expo-router/drawer";
+import { Redirect } from "expo-router";
+import { getToken } from "../services/storageService";
 
 export default function RootLayout() {
+  const [token, setToken] = useState<string | null | undefined>(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const t = await getToken();
+      setToken(t ?? null);
+    })();
+  }, []);
+
+  if (token === undefined) return null;       // Espera a leer el token
+  if (!token) return <Redirect href="/login" />; // Sin token → login
+
   return (
     <Drawer screenOptions={{ headerShown: true }}>
-      <Drawer.Screen
-        name="index"
-        options={{ title: "Portada" }}
-          
-        
-      />
-
-      <Drawer.Screen
-        name="tabs"
-        options={{ title: "Portfolio" }}
-        // para ocultar un archivo seria
-        // drawerItemStyle = {Display: 'none'}
-      />
-
-      <Drawer.Screen
-        name="list"
-        options={{ title: "Lista de Animes" }}
-      />
+      <Drawer.Screen name="index" options={{ title: "Portada" }} />
+      <Drawer.Screen name="tabs" options={{ title: "Portfolio" }} />
+      <Drawer.Screen name="list" options={{ title: "Lista de Animes" }} />
     </Drawer>
   );
 }
